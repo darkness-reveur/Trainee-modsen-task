794f1d55f7da59704ea324243401eb4adc8d9dcf
satma0745 <catman0745@gmail.com>
2022-03-26 14:15:29 +0300

    Fix private fields names.

--- MeetupPlatformApi/Controllers/MeetupsController.cs
+++ MeetupPlatformApi/Controllers/MeetupsController.cs
@@ -11,67 +11,67 @@ namespace MeetupPlatformApi.Controllers;
 [ApiController]
 public class MeetupsController : ControllerBase
 {
-    private readonly ApplicationContext _context;
-    private readonly IMapper _mapper;
+    private readonly ApplicationContext context;
+    private readonly IMapper mapper;
 
     public MeetupsController(ApplicationContext context, IMapper mapper)
     {
-        _context = context;
-        _mapper = mapper;
+        this.context = context;
+        this.mapper = mapper;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAllMeetups()
     {
-        var meetups = await _context.Meetups.ToListAsync();
-        var outputDtos = _mapper.Map<IEnumerable<MeetupOutputDto>>(meetups);
+        var meetups = await context.Meetups.ToListAsync();
+        var outputDtos = mapper.Map<IEnumerable<MeetupOutputDto>>(meetups);
         return Ok(outputDtos);
     }
 
     [HttpGet("{id:int}")]
     public async Task<IActionResult> GetMeetupById([FromRoute] int id)
     {
-        var meetup = await _context.Meetups.SingleOrDefaultAsync(meetup => meetup.Id == id);
-        var outputDto = _mapper.Map<MeetupOutputDto>(meetup);
+        var meetup = await context.Meetups.SingleOrDefaultAsync(meetup => meetup.Id == id);
+        var outputDto = mapper.Map<MeetupOutputDto>(meetup);
         return outputDto is not null ? Ok(outputDto) : NotFound();
     }
 
     [HttpPost]
     public async Task<IActionResult> AddMeetup([FromBody] MeetupInputDto inputDto)
     {
-        var meetup = _mapper.Map<MeetupEntity>(inputDto);
-        await _context.Meetups.AddAsync(meetup);
-        await _context.SaveChangesAsync();
+        var meetup = mapper.Map<MeetupEntity>(inputDto);
+        await context.Meetups.AddAsync(meetup);
+        await context.SaveChangesAsync();
 
-        var outputDto = _mapper.Map<MeetupOutputDto>(meetup);
+        var outputDto = mapper.Map<MeetupOutputDto>(meetup);
         return CreatedAtAction(nameof(GetMeetupById), new { id = outputDto.Id }, outputDto);
     }
 
     [HttpPut("{id:int}")]
     public async Task<IActionResult> UpdateMeetup([FromRoute] int id, [FromBody] MeetupInputDto inputDto)
     {
-        var meetup = await _context.Meetups.SingleOrDefaultAsync(meetup => meetup.Id == id);
+        var meetup = await context.Meetups.SingleOrDefaultAsync(meetup => meetup.Id == id);
         if (meetup is null)
         {
             return NotFound();
         }
 
-        _mapper.Map(inputDto, meetup);
-        await _context.SaveChangesAsync();
+        mapper.Map(inputDto, meetup);
+        await context.SaveChangesAsync();
         return NoContent();
     }
 
     [HttpDelete("{id:int}")]
     public async Task<IActionResult> DeleteUserById([FromRoute] int id)
     {
-        var exMeetupEntity = await _context.Meetups.SingleOrDefaultAsync(meetup => meetup.Id == id);
+        var exMeetupEntity = await context.Meetups.SingleOrDefaultAsync(meetup => meetup.Id == id);
         if (exMeetupEntity is null)
         {
             return NotFound();
         }
 
-        _context.Meetups.Remove(exMeetupEntity);
-        await _context.SaveChangesAsync();
+        context.Meetups.Remove(exMeetupEntity);
+        await context.SaveChangesAsync();
         return NoContent();
     }
 }
