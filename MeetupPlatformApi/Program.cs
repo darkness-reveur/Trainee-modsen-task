e13d2d1f7dc11887585cca519a6696793abc8be9
satma0745 <catman0745@gmail.com>
2022-03-26 14:01:15 +0300

    Restructure configuration inside the Program.cs file.

--- MeetupPlatformApi/Program.cs
+++ MeetupPlatformApi/Program.cs
@@ -3,21 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddAutoMapper(typeof(Program));
-
 builder.Services.AddDbContext<MeetupsPlatformContext>(options =>
-    options.UseInMemoryDatabase(databaseName: "Test"));
+{
+    options.UseInMemoryDatabase(databaseName: "MeetupPlatform");
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -25,7 +21,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
