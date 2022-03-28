3bd56903330e685e06fc41613e0084e050e7e1f1
darkness-reveur <uvapuvp@mail.ru>
2022-03-28 20:45:15 +0300

    Add validation for DTOs, fixed .gitignore file
    Added [Required] and [MaxLength()] attributes

--- MeetupPlatformApi/DataTransferObjects/MeetupInputDto.cs
+++ MeetupPlatformApi/DataTransferObjects/MeetupInputDto.cs
@@ -1,12 +1,20 @@
-﻿namespace MeetupPlatformApi.DataTransferObjects;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetupPlatformApi.DataTransferObjects;
 
 public class MeetupInputDto
 {
+    [Required]
+    [MaxLength(30)]
     public string Name { get; set; }
 
+    [Required]
     public DateTime StartTime { get; set; }
 
+    [Required]
     public DateTime EndTime { get; set; }
 
+    [Required]
+    [MaxLength(300)]
     public string Description { get; set; }
 }
--- MeetupPlatformApi/DataTransferObjects/MeetupOutputDto.cs
+++ MeetupPlatformApi/DataTransferObjects/MeetupOutputDto.cs
@@ -1,14 +1,23 @@
-﻿namespace MeetupPlatformApi.DataTransferObjects;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetupPlatformApi.DataTransferObjects;
 
 public class MeetupOutputDto
 {
+    [Required]
     public int Id { get; set; }
 
+    [Required]
+    [MaxLength(30)]
     public string Name { get; set; }
 
+    [Required]
     public DateTime StartTime { get; set; }
 
+    [Required]
     public DateTime EndTime { get; set; }
 
+    [Required]
+    [MaxLength(300)]
     public string Description { get; set; }
 }
