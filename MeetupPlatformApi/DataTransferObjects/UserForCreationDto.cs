9d2e9e129df44f24331db7826928dcddd413193f
AlexanderKarcheuski <karchevskji@gmail.com>
2022-03-28 15:19:32 +0300

    Created UserForCreationDto as data transfer object

---
+++ MeetupPlatformApi/DataTransferObjects/UserForCreationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MeetupPlatformApi.DataTransferObjects
+{
+    public class UserForCreationDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
