e4b6cb085a9626a75c11903b4b249f2e726ac578
darkness-reveur <uvapuvp@mail.ru>
2022-03-28 16:23:53 +0300

    Remuved DataTypes, fixed gitignore

--- MeetupPlatformApi/DataTransferObjects/MeetupInputDto.cs
+++ MeetupPlatformApi/DataTransferObjects/MeetupInputDto.cs
@@ -6,18 +6,15 @@ public class MeetupInputDto
 {
     [Required]
     [MaxLength(30)]
-    [DataType(DataType.Text)]
     [Display(Name = "Name of meetup")]
     public string Name { get; set; }
 
     [Required]
-    [DataType(DataType.DateTime)]
     public DateTime StartTime { get; set; }
 
-    [DataType(DataType.DateTime)]
-    public DateTime? EndTime { get; set; }
+    [Required]
+    public DateTime EndTime { get; set; }
 
-    [DataType(DataType.Text)]
     [MaxLength(300)]
     [Display(Name = "Meetup description")]
     public string Description { get; set; }
--- MeetupPlatformApi/DataTransferObjects/MeetupOutputDto.cs
+++ MeetupPlatformApi/DataTransferObjects/MeetupOutputDto.cs
@@ -9,20 +9,17 @@ public class MeetupOutputDto
 
     [Required]
     [MaxLength(30)]
-    [DataType(DataType.Text)]
     [Display(Name = "Name of meetup")]
     public string Name { get; set; }
 
     [Required]
-    [DataType(DataType.DateTime)]
     public DateTime StartTime { get; set; }
 
-    [DataType(DataType.DateTime)]
+    [Required]
     public DateTime EndTime { get; set; }
 
-    [Display(Name = "Meetup description")]
-    [DataType(DataType.Text)]
     [MaxLength(300)]
+    [Display(Name = "Meetup description")]
     public string Description { get; set; }
 }
 
