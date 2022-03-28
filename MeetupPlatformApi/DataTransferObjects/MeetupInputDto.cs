19f71b51a063b3dcc46a9e0bd2b52766b9cab42b
uvapuvp@mail.ru <vlad5239>
2022-03-28 15:36:02 +0300

    Added validation for DTOs
    data annatations

--- MeetupPlatformApi/DataTransferObjects/MeetupInputDto.cs
+++ MeetupPlatformApi/DataTransferObjects/MeetupInputDto.cs
@@ -1,13 +1,25 @@
-﻿namespace MeetupPlatformApi.DataTransferObjects;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetupPlatformApi.DataTransferObjects;
 
 public class MeetupInputDto
 {
+    [Required]
+    [MaxLength(30)]
+    [DataType(DataType.Text)]
+    [Display(Name = "Name of meetup")]
     public string Name { get; set; }
 
+    [Required]
+    [DataType(DataType.DateTime)]
     public DateTime StartTime { get; set; }
 
-    public DateTime EndTime { get; set; }
+    [DataType(DataType.DateTime)]
+    public DateTime? EndTime { get; set; }
 
+    [DataType(DataType.Text)]
+    [MaxLength(300)]
+    [Display(Name = "Meetup description")]
     public string Description { get; set; }
 }
 
--- MeetupPlatformApi/DataTransferObjects/MeetupOutputDto.cs
+++ MeetupPlatformApi/DataTransferObjects/MeetupOutputDto.cs
@@ -1,15 +1,28 @@
-﻿namespace MeetupPlatformApi.DataTransferObjects;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetupPlatformApi.DataTransferObjects;
 
 public class MeetupOutputDto
 {
+    [Required]
     public int Id { get; set; }
 
+    [Required]
+    [MaxLength(30)]
+    [DataType(DataType.Text)]
+    [Display(Name = "Name of meetup")]
     public string Name { get; set; }
 
+    [Required]
+    [DataType(DataType.DateTime)]
     public DateTime StartTime { get; set; }
 
+    [DataType(DataType.DateTime)]
     public DateTime EndTime { get; set; }
 
+    [Display(Name = "Meetup description")]
+    [DataType(DataType.Text)]
+    [MaxLength(300)]
     public string Description { get; set; }
 }
 
