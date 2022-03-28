d18051abf6390f4f45dac2c6c91a1e45962bcd71
Vladislav <55953642+darkness-reveur@users.noreply.github.com>
2022-03-28 23:21:44 +0300

    Revert "Add validation for DTOs, fixed .gitignore file"

--- MeetupPlatformApi/DataTransferObjects/MeetupInputDto.cs
+++ MeetupPlatformApi/DataTransferObjects/MeetupInputDto.cs
@@ -1,20 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace MeetupPlatformApi.DataTransferObjects;
+﻿namespace MeetupPlatformApi.DataTransferObjects;
 
 public class MeetupInputDto
 {
-    [Required]
-    [MaxLength(200)]
     public string Name { get; set; }
 
-    [Required]
     public DateTime StartTime { get; set; }
 
-    [Required]
     public DateTime EndTime { get; set; }
 
-    [Required]
-    [MaxLength(4000)]
     public string Description { get; set; }
 }
--- MeetupPlatformApi/DataTransferObjects/MeetupOutputDto.cs
+++ MeetupPlatformApi/DataTransferObjects/MeetupOutputDto.cs
@@ -1,21 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace MeetupPlatformApi.DataTransferObjects;
+﻿namespace MeetupPlatformApi.DataTransferObjects;
 
 public class MeetupOutputDto
 {
-    [Required]
     public int Id { get; set; }
 
-    [Required]
     public string Name { get; set; }
 
-    [Required]
     public DateTime StartTime { get; set; }
 
-    [Required]
     public DateTime EndTime { get; set; }
 
-    [Required]
     public string Description { get; set; }
 }
