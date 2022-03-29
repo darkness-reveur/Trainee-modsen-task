619fb65967d543d9e3c8871ed874bf0741a1d376
AlexanderKarcheuski <karchevskji@gmail.com>
2022-03-29 13:51:56 +0300

    Created util for hash password

---
+++ MeetupPlatformApi/Utils/HashHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace MeetupPlatformApi.Utils
+{
+    public static class HashHandler
+    {
+        public static string HashPassword(string password)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rngCsp = new RNGCryptoServiceProvider())
+            {
+                rngCsp.GetNonZeroBytes(salt);
+            }
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8));
+
+            return hashed;
+        }
+    }
+}
