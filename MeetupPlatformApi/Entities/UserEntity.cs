dfefdc3a519ba7b5e611c9b36059543b8cd8ed89
AlexanderKarcheuski <karchevskji@gmail.com>
2022-03-30 15:18:11 +0300

    Added annotations for user entity

--- MeetupPlatformApi/Entities/UserEntity.cs
+++ MeetupPlatformApi/Entities/UserEntity.cs
@@ -1,10 +1,16 @@
-﻿namespace MeetupPlatformApi.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetupPlatformApi.Entities;
 
 public class UserEntity
 {
+    [Required]
+    [Key]
     public Guid Id { get; set; }
 
+    [Required]
     public string Username { get; set; }
 
+    [Required]
     public string Password { get; set; }
 }
