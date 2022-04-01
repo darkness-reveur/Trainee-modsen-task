8e696f7cdba0e1f15bd972dd0aa3801dc3f37e0c
AlexanderKarcheuski <karchevskji@gmail.com>
2022-04-01 14:32:37 +0300

    Created dto for change credentials of user

---
+++ MeetupPlatformApi/DataTransferObjects/UserChangeCredentialsDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetupPlatformApi.DataTransferObjects
+{
+    public class UserChangeCredentialsDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string OldPassword { get; set; }
+
+        [Required]
+        public string NewPassword { get; set; }
+    }
+}
