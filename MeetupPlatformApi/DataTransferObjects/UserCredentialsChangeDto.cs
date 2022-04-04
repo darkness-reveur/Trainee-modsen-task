61de2ab814097c69bb24db205c547f31ba46bb09
AlexanderKarcheuski <karchevskji@gmail.com>
2022-04-04 08:47:15 +0300

    Added file-scoped namespaces

--- MeetupPlatformApi/DataTransferObjects/UserCredentialsChangeDto.cs
+++ MeetupPlatformApi/DataTransferObjects/UserCredentialsChangeDto.cs
@@ -1,17 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿namespace MeetupPlatformApi.DataTransferObjects;
 
-namespace MeetupPlatformApi.DataTransferObjects
+using System.ComponentModel.DataAnnotations;
+
+public class UserCredentialsChangeDto
 {
-    public class UserCredentialsChangeDto
-    {
-        [Required]
-        public string Username { get; set; }
+    [Required]
+    public string Username { get; set; }
 
-        [Required]
-        public string OldPassword { get; set; }
+    [Required]
+    public string OldPassword { get; set; }
 
-        [Required]
-        [MaxLength(30)]
-        public string NewPassword { get; set; }
-    }
+    [Required]
+    [MaxLength(30)]
+    public string NewPassword { get; set; }
 }
