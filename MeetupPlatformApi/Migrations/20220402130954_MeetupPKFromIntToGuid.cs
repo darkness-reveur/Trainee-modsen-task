c7b70f91c23e804c1368cdd4db79a3901b433b67
AlexanderKarcheuski <karchevskji@gmail.com>
2022-04-04 17:33:26 +0300

    Added old migrations

---
+++ MeetupPlatformApi/Migrations/20220402130954_MeetupPKFromIntToGuid.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MeetupPlatformApi.Migrations
+{
+    public partial class MeetupPKFromIntToGuid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_meetups",
+                table: "meetups");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "meetups");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "id",
+                table: "meetups",
+                nullable: false,
+                defaultValueSql: "gen_random_uuid()");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_meetups",
+                table: "meetups",
+                column: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_meetups",
+                table: "meetups");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "meetups");
+
+            migrationBuilder.AddColumn<int>(
+                name: "id",
+                table: "meetups",
+                nullable: false)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_meetups",
+                table: "meetups",
+                column: "id");
+        }
+    }
+}
