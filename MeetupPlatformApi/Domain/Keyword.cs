d8ecf39b47d81cc0151a4d7b268feff953ae8ec9
Vladislav <uvapuvp@mail.ru>
2022-04-11 18:02:39 +0300

    Added Keyword and intermediate table models

---
+++ MeetupPlatformApi/Domain/Keyword.cs
@@ -0,0 +1,8 @@
+﻿namespace MeetupPlatformApi.Domain;
+
+public class Keyword
+{
+    public Guid Id { get; set; }
+
+    public string KeywordName { get; set; }
+}
---
+++ MeetupPlatformApi/Domain/MeetupKeyword.cs
@@ -0,0 +1,9 @@
+﻿namespace MeetupPlatformApi.Domain;
+
+public class MeetupKeyword
+{
+    public int MeetupId { get; set; }
+
+    public int KeywordId { get; set; }
+}
+
