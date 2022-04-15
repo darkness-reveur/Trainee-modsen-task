df596af5e796d3be5132cc52ed5c2cfc779d82a7
AlexanderKarcheuski <karchevskji@gmail.com>
2022-04-15 11:49:58 +0300

    Created get all comments feature

---
+++ MeetupPlatform.Api/Features/Meetups/GetRootComments/GetRootCommentsFeature.cs
@@ -0,0 +1,53 @@
+﻿namespace MeetupPlatform.Api.Features.Meetups.GetRootComments;
+
+using AutoMapper;
+using MeetupPlatform.Api.Authentication.Helpers;
+using MeetupPlatform.Api.Persistence.Context;
+using MeetupPlatform.Api.Seedwork.WebApi;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiSection(ApiSections.Meetups)]
+public class GetRootCommentsFeature : FeatureBase
+{
+    private readonly ApplicationContext context;
+    private readonly IMapper mapper;
+
+    public GetRootCommentsFeature(ApplicationContext context, IMapper mapper)
+    {
+        this.context = context;
+        this.mapper = mapper;
+    }
+
+    /// <summary>
+    /// Get all comments of meetup
+    /// </summary>
+    /// <response code="404">If needed meetup is null.</response>
+    /// <response code="200">Returns meetup's list of comments.</response>
+    [HttpGet("/api/meetups/{id:guid}/comments")]
+    [Authorize(Roles = Roles.PlainUser)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(RootCommentInfoDto), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetRootComments([FromRoute] Guid id)
+    {
+        var meetup = await context.Meetups
+            .Include(meetup => meetup.RootComments)
+            .Include(meetup => meetup.SignedUpUsers)
+            .Where(meetup => meetup.Id == id)
+            .SingleOrDefaultAsync();
+        if(meetup is null)
+        {
+            return NotFound();
+        }
+
+        bool isUserSignedUpForMeetup = meetup.SignedUpUsers.Any(plainUser => plainUser.Id == CurrentUser.UserId);
+        if(!isUserSignedUpForMeetup)
+        {
+            return Forbid();
+        }
+
+        var rootCommentInfoDtos = mapper.Map<List<RootCommentInfoDto>>(meetup.RootComments);
+        return Ok(rootCommentInfoDtos);
+    }
+}
---
+++ MeetupPlatform.Api/Features/Meetups/GetRootComments/RootCommentInfoDto.cs
@@ -0,0 +1,20 @@
+﻿namespace MeetupPlatform.Api.Features.Meetups.GetRootComments;
+
+using System.ComponentModel.DataAnnotations;
+
+public class RootCommentInfoDto
+{
+    /// <summary>
+    /// Comment id value
+    /// </summary>
+    /// <example>cc3e5bce-a8c0-4258-b663-71ec8f2b6446</example>
+    [Required]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Comment text
+    /// </summary>
+    /// <example>You're gorgeous!</example>
+    [Required]
+    public string Text { get; set; }
+}
